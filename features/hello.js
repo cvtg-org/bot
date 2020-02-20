4380df22f8489578899b6739ec378cc2f37415da
cyashley <48338718+cyashley@users.noreply.github.com>
2020-02-19 21:39:41 -0500

    Update hello.js

--- features/hello.js
+++ features/hello.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 
 module.exports = function( controller ) {
 
-    controller.hears( [ 'hi','hello','howdy','hey','aloha','hola','bonjour','oi' ], 'message,direct_message', async ( bot,message ) => {
+    controller.hears( [ 'hi','hello','howdy','hey','aloha','hola','bonjour','oi','whats up', 'yo' ], 'message,direct_message', async ( bot,message ) => {
 
         await bot.reply( message,'Greetings!' );
         await bot.reply( message, { markdown: 'Try `help` to see available commands' } );
